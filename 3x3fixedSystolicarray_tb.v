dcd6562654ae7169451711c515a1863e1ff76aee
youssefsameh99 <ysameh487@gmail.com>
2025-07-26 04:16:29 +0300

    Update 3x3fixedSystolicarray_tb.v

--- 3x3fixedSystolicarray_tb.v
+++ 3x3fixedSystolicarray_tb.v
@@ -1,5 +1,5 @@
  module test;
- // Inputs
+
  reg clk;
  reg reset;
  reg [7:0] a1;
@@ -8,7 +8,8 @@
  reg [7:0] b1;
  reg [7:0] b2;
  reg [7:0] b3;
- // Outputs
+ reg valid_in;
+ 
  wire [16:0] c1;
  wire [16:0] c2;
  wire [16:0] c3;
@@ -18,9 +19,9 @@
  wire [16:0] c7;
  wire [16:0] c8;
  wire [16:0] c9;
+wire valid_out;
 
-
- // Instantiate the Unit Under Test (UUT)
+ 
  top uut (
  .clk(clk), 
 .reset(reset), 
@@ -42,13 +43,15 @@
  );
 
  initial begin
- // Initialize Inputs
+
  clk = 0;
  reset = 0;
+ valid_in = 0;
  a1 = 0;   a2 = 0;   a3 = 0;   b1 = 0;   b2 = 0;   b3 = 0;
  #5 reset = 1;
  #5 reset = 0;
  @(negedge clk);
+ valid_in = 1;
  a1 = 8'h01;   a2 = 8'h04;   a3 = 8'h07;   b1 = 8'h02;   b2 = 8'h01;   b3 = 8'h03;
  @(negedge clk);
   a1 = 8'h02;   a2 = 8'h05;   a3 = 8'h08;   b1 = 8'h04;   b2 = 8'h05;   b3 = 8'h07;
@@ -56,11 +59,14 @@
      a1 = 8'h03;   a2 = 8'h06;   a3 = 8'h09;   b1 = 8'h06;   b2 = 8'h09;   b3 = 8'h08;
      @(negedge clk);
      a1 = 0;   a2 = 0;   a3 = 0;   b1 = 0;   b2 = 0;   b3 = 0;
+     valid_in = 0;
  
 
  #100;
  $stop;
  end
+
+ 
  initial begin
  forever #1 clk = ~clk;
  end
