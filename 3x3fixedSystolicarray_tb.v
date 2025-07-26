081ea603a6e810280e63840c23ede93129386e21
youssefsameh99 <ysameh487@gmail.com>
2025-07-26 06:45:52 +0300

    Update 3x3fixedSystolicarray_tb.v

--- 3x3fixedSystolicarray_tb.v
+++ 3x3fixedSystolicarray_tb.v
@@ -1,45 +1,55 @@
-module test();
-reg clk, reset;
-reg [23:0] matrix_a_in, matrix_b_in;
-reg valid_in; 
-wire valid_out;
-wire [144:0] matrix_c_out;
-top uut (
-    .clk(clk),
-    .reset(reset),
-    .matrix_a_in(matrix_a_in),
-    .matrix_b_in(matrix_b_in),
-    .valid_in(valid_in),
-    .valid_out(valid_out),
-    .matrix_c_out(matrix_c_out)
-);
-initial begin
-  clk = 0;
-  forever #1 clk = ~clk; // Clock period of 2 time units
-end
+ module test;
 
-initial begin
-  reset = 1;
-  valid_in = 0;
-  matrix_a_in = 24'h000000; // Initialize to zero
-  matrix_b_in = 24'h000000; // Initialize to zero
-  
- reset = 1; // Release reset after 5 time units
-  @(negedge clk);
-  valid_in = 1; // Indicate valid input
-  reset = 0; // Assert reset again for a short duration
-  matrix_a_in = 24'h741; // Example values for matrix A
-  matrix_b_in = 24'h312; // Example values for matrix B
-  @(negedge clk);
-    matrix_a_in = 24'h852; // Example values for matrix A
-  matrix_b_in = 24'h754; // Example values for matrix B
-  @(negedge clk);
-  matrix_a_in = 24'h963; // Example values for matrix A
-  matrix_b_in = 24'h896; // Example values for matrix B
-  @(negedge clk);
-  valid_in = 0; // Clear valid input after processing
-  repeat(10) @(negedge clk); // Wait for some cycles to observe outputs
-  
-  $stop; // End simulation
-end
-endmodule
+ reg clk;
+ reg reset;
+reg [23:0] matrix_a_in, matrix_b_in;
+ reg valid_in;
+ 
+wire [47:0] matrix_c_out;
+
+ 
+ top uut (
+ .clk(clk), 
+.reset(reset), 
+.matrix_a_in(matrix_a_in),
+.matrix_b_in(matrix_b_in),
+.valid_in(valid_in),
+.valid_out(valid_out),
+.matrix_c_out(matrix_c_out)
+ );
+
+ initial begin
+ reset = 0;
+ valid_in = 0;
+      valid_in = 0;
+     matrix_a_in = 24'h000000; // Initialize to zero
+     matrix_b_in = 24'h000000; // Initialize to zero    
+ @(posedge clk); reset = 1;
+ @(posedge clk); reset = 0;
+     valid_in = 1; // Indicate valid input
+     matrix_a_in = 24'h070401; // Example values for matrix A
+     matrix_b_in = 24'h030102; // Example values for matrix B
+     @(posedge clk);
+        matrix_a_in = 24'h080502; // Example values for matrix A
+        matrix_b_in = 24'h070504; // Example values for matrix B
+        @(posedge clk);
+        matrix_a_in = 24'h090603; // Example values for matrix A
+        matrix_b_in = 24'h080906; // Example values for matrix B
+        @(posedge clk); // Clear valid input after processing
+        valid_in = 0; // Indicate valid input
+         matrix_a_in = 24'h000000; // Example values for matrix A
+        matrix_b_in = 24'h000000;
+
+        repeat(10) @(posedge clk); // Wait for some cycles to observe outputs
+ 
+
+ #100;
+ $stop;
+ end
+
+
+ initial begin
+     clk = 0;
+ forever #1 clk = ~clk;
+ end
+ endmodule
