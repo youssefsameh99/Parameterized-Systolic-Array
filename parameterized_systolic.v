3fe0e506769474324e1acd6dcd96036090fd7b45
youssefsameh99 <ysameh487@gmail.com>
2025-07-27 06:20:57 +0300

    Update parameterized_systolic.v

--- parameterized_systolic.v
+++ parameterized_systolic.v
@@ -1,104 +1,44 @@
-module systolic_array (clk,rst_n,valid_in,matrix_a_in,matrix_b_in,valid_out,matrix_c_out);
-parameter DATAWIDTH = 8;
-parameter N_SIZE = 3;
-input clk,rst_n,valid_in;
-input [N_SIZE*DATAWIDTH-1:0] matrix_a_in,matrix_b_in;
-output reg valid_out;
-output reg [N_SIZE*2*DATAWIDTH-1:0] matrix_c_out;
-wire [DATAWIDTH-1:0] a_delayed[0:N_SIZE][0:N_SIZE];
-wire [DATAWIDTH-1:0] b_delayed[0:N_SIZE][0:N_SIZE];
+module systolic_array #(
+parameter DATAWIDTH = 8,
+parameter N_SIZE = 3)(
+input clk,rst_n,valid_in,
+input [N_SIZE*DATAWIDTH-1:0] matrix_a_in,matrix_b_in,
+output reg valid_out,
+output reg [N_SIZE*2*DATAWIDTH-1:0] matrix_c_out
+);
 
 
+wire [DATAWIDTH-1:0] a_reg[0:N_SIZE-1];
+wire [DATAWIDTH-1:0] b_reg[0:N_SIZE-1];
+assign a_reg[0]=matrix_a_in[DATAWIDTH-1 : 0];
+assign b_reg[0]=matrix_b_in[DATAWIDTH-1 : 0];
 
+genvar i, j;
+generate
+ for ( i=1;i<N_SIZE;i=i+1) begin
+ REG #(.WIDTH(DATAWIDTH),.DELAY_STAGES(i)) A(clk,rst_n,matrix_a_in[(i+1)*DATAWIDTH-1 -: DATAWIDTH],a_reg[i]);  
+ end   
+ for(j=1;j<N_SIZE;j=j+1) begin
+ REG #(.WIDTH(DATAWIDTH),.DELAY_STAGES(j)) B(clk,rst_n,matrix_b_in[(j+1)*DATAWIDTH-1 -: DATAWIDTH],b_reg[j]);  
+ end
+endgenerate
 
-assign a_delayed[0][0]=matrix_a_in[DATAWIDTH-1 : 0];
-assign b_delayed[0][0]=matrix_b_in[DATAWIDTH-1 : 0];
-
-
-genvar j,i;
-
+wire   [2*DATAWIDTH-1:0] c [0:N_SIZE-1][0:N_SIZE-1];
+wire   [DATAWIDTH-1:0] a_bet_pe     [0:N_SIZE-1][0:N_SIZE-1];
+wire   [DATAWIDTH-1:0] b_bet_pe      [0:N_SIZE-1][0:N_SIZE-1];
 
 
 generate
-    for (i = 1; i < N_SIZE; i = i + 1) begin : gen_a_delay
-        REG #(
-            .WIDTH(DATAWIDTH),
-            .DELAY(i)
-        ) A (
-            .clk(clk),
-            .rst_n(rst_n),
-            .d(matrix_a_in[(i+1)*DATAWIDTH-1 -: DATAWIDTH]),
-            .q(a_delayed[i][0])
-        );
-    end
-    for (j = 1; j < N_SIZE; j = j + 1) begin : gen_b_delay
-        REG #(
-            .WIDTH(DATAWIDTH),
-            .DELAY(j)
-        ) B (
-            .clk(clk),
-            .rst_n(rst_n),
-            .d(matrix_b_in[(j+1)*DATAWIDTH-1 -: DATAWIDTH]),
-            .q(b_delayed[0][j])
-        );
+    for (i = 0; i < N_SIZE; i = i + 1) begin 
+    for (j = 0; j < N_SIZE; j = j + 1) begin 
+ PE #(.data_size(DATAWIDTH)) pe(clk,rst_n,(j == 0) ? a_reg[i] : a_bet_pe[i][j-1] ,(i == 0) ? b_reg[j] : b_bet_pe[i-1][j]
+    , c[i][j], a_bet_pe[i][j],b_bet_pe[i][j]);
+        end
     end
 endgenerate
 
 
 
-    wire [2*DATAWIDTH-1:0] c_wires [0:N_SIZE*2-1][0:N_SIZE*2-1];
-    generate
-        for (i = 0; i < N_SIZE; i = i + 1) begin : ROW
-            for (j = 0; j < N_SIZE; j = j + 1) begin : COL
-                PE PE_inst (
-                    .clk(clk),
-                    .reset(rst_n),
-                    .in_a(a_delayed[i][j]),
-                    .in_b(b_delayed[i][j]),
-                    .out_a(a_delayed[i][j+1]),
-                    .out_b(b_delayed[i+1][j]),
-                    .out_c(c_wires[i][j])
-                );
-            end
-        end
-    endgenerate
-
-reg [2*N_SIZE:0] valid_counter;
-always @(posedge clk) begin
-    if (rst_n) begin
-        valid_counter <= 0;
-    end else begin
-        if (valid_counter < 2*N_SIZE+1) valid_counter <= valid_counter + 1;
-    end
-end
-
-always @(posedge clk) begin
-    if (rst_n)
-        valid_out <= 0;
-    else begin
-        valid_out <= (valid_counter >= N_SIZE+1 && valid_counter <= 2*N_SIZE);
-    end
-end
-
-
-integer row, col;
-always @(posedge clk) begin // Change to sequential for better timing
-    matrix_c_out <= 0;
-    if (valid_counter >= N_SIZE + 2 && valid_counter <= 2 * N_SIZE + 1) begin
-        row = valid_counter - (N_SIZE + 2);
-        for (col = 0; col < N_SIZE; col = col + 1) begin
-            matrix_c_out[(col*2*DATAWIDTH) +: 2*DATAWIDTH] <= c_wires[row][col];
-        end
-    end
-end
-
-
-
-
-
-
-
-
 
 
 endmodule
@@ -107,47 +47,42 @@ endmodule
 
 
 
-
-
-module REG (clk,rst_n,d,q);
-    parameter WIDTH = 8;
-    parameter DELAY = 1;
-    input wire clk;
-    input wire rst_n;
-    input wire [WIDTH-1:0] d;
-    output wire [WIDTH-1:0] q;
-    reg [WIDTH-1:0] shift_reg [0:DELAY-1];
+module REG (clk,rst_n,in,q);
+    parameter WIDTH = 8;              
+    parameter DELAY_STAGES = 1;              
+    input clk,rst_n;
+    input  [WIDTH-1:0] in;
+    output [WIDTH-1:0] q;
+    reg [WIDTH-1:0] Register [0:DELAY_STAGES-1];  
     integer i;
     always @(posedge clk or negedge rst_n) begin
-        if (rst_n) begin
-            for (i = 0; i < DELAY; i = i + 1)
-                shift_reg[i] <= 0;
+        if (!rst_n) begin
+            for (i = 0; i < DELAY_STAGES; i = i + 1)
+                Register[i] <= 0;
         end
          else begin
-            shift_reg[0] <= d;
-            for (i = 1; i < DELAY; i = i + 1)
-            shift_reg[i] <= shift_reg[i-1];
+            Register[0] <= in;
+            for (i = 1; i < DELAY_STAGES; i = i + 1)
+            Register[i] <= Register[i-1];
         end
     end
-    assign q = shift_reg[DELAY-1];
+    assign q = Register[DELAY_STAGES-1];
 endmodule
 
 
-module PE#(parameter data_size=8)(
- input wire reset,clk,
+ module PE#(parameter data_size=8,parameter N_SIZE = 3)(
+ input wire clk,rst_n,
  input wire [data_size-1:0] in_a,in_b,
  output reg [2*data_size-1:0] out_c,
  output reg [data_size-1:0] out_a,out_b
  );
- 
-
  always @(posedge clk)begin
- if(reset) begin
+ if(!rst_n) begin
  out_a <= 0;
  out_b <= 0;
  out_c <= 0;
- end else begin 
-out_c <= out_c + in_a*in_b;
+ end else begin
+ out_c <= out_c + in_a*in_b;
  out_a <=in_a;
  out_b <=in_b;
  end
